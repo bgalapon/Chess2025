68e65100291723f5f8862e4eed5bfbc04e81511b
Bryson Galapon <brysongalapon@gmail.com>
2025-09-04 19:49:44 -0700

    Test some enpassent

--- main.cpp
+++ main.cpp
@@ -634,5 +634,21 @@ std::unique_ptr<Board> StandardBoard() {
 int main() {
     std::unique_ptr<Board> board = StandardBoard();
     std::cout << board->toString() << std::endl;
+
+    board->move(Square::E2, Square::E4);
+    std::cout << board->toString() << std::endl;
+
+    board->move(Square::D7, Square::D5);
+    std::cout << board->toString() << std::endl;
+
+    board->move(Square::E4, Square::D5);
+    std::cout << board->toString() << std::endl;
+
+    board->move(Square::C7, Square::C5);
+    std::cout << board->toString() << std::endl;
+
+    board->move(Square::D5, Square::C6);
+    std::cout << board->toString() << std::endl;
+
     return 0;
 }
